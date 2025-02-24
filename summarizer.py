275a86b797410e5241dad7f4b3829abb58522d94
Bodruzzaman Ansari NAFI <101050010+nafi001@users.noreply.github.com>
2025-02-24 12:03:33 +0600

    Create summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,6 @@
+pip install   transformers
+from transformers import pipeline
+
+def summarize_text(text, max_length=150):
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    return summarizer(text, max_length=max_length, min_length=30, do_sample=False)[0]['summary_text']
