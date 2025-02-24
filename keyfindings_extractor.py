868a59375d8d4d44dafa8443213eb9908f745fe7
Bodruzzaman Ansari NAFI <101050010+nafi001@users.noreply.github.com>
2025-02-24 12:04:57 +0600

    Create keyfindings_extractor.py

---
+++ keyfindings_extractor.py
@@ -0,0 +1,7 @@
+pip install  rake-nltk pybtex
+from rake_nltk import Rake
+
+def extract_key_findings(text, num_points=5):
+    r = Rake()
+    r.extract_keywords_from_text(text)
+    return r.get_ranked_phrases()[:num_points]
